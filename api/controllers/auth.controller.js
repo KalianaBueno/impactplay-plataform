5b58de4ae70d3ce745efc51ecb2608991a48419a
Kaliana Bueno <kalianabueno@MacBook-Air-de-Kaliana.local>
2024-06-08 19:22:08 -0300

    middleware e funcao para erros

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || password === '') {
-        return res.status(400).json({message: 'Campos obrigatórios'});
+        next(errorHandler(400, 'campos obrigatórios'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -20,7 +21,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json('sucesso no signup');
     } catch(error) {
-        res.status(500).json({message: error.message});
+        next(error);
     }
 
 };
