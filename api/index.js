3d19c9494c8621d2d61fb6106257b54dec6c5f2d
Kaliana Bueno <kalianabueno@MacBook-Air-de-Kaliana.local>
2024-06-08 17:25:15 -0300

    conexao com banco de dados

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('mongodb está conectado');
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('server está rodando na porta 3000');
 })
