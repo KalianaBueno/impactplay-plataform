6babb93dd710ffca3bafd5e18d7b3f771dd72ba0
Kaliana Bueno <kalianabueno@MacBook-Air-de-Kaliana.local>
2024-06-11 20:27:54 -0300

    pre instalacao

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 
 dotenv.config();
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO)
 }).catch((err) => {
     console.log(err);
 });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -28,6 +31,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) =>{
     const statusCode = err.statusCode || 500;
